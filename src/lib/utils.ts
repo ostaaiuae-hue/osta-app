08d341d89349f55adaee851de68e30dfe57d3fc4
ostaaiuae-hue <ostaaiuae@gmail.com>
2026-07-15 01:23:01 +0400

    utils: add utility functions

---
+++ src/lib/utils.ts
@@ -0,0 +1,18 @@
+export const formatDate = (date: Date): string => {
+  return new Date(date).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+};
+
+export const formatTime = (date: Date): string => {
+  return new Date(date).toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+};
+
+export const cn = (...classes: (string | undefined | null | false)[]): string => {
+  return classes.filter(Boolean).join(' ');
+};
